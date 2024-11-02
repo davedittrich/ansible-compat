1b0b6cccf797000aeb02a5316899dbfd8bc77aaa
Dave Dittrich <dave.dittrich@gmail.com>
2024-11-02 15:36:39 -0700

    Ensure Path object before calling resolve()

--- src/ansible_compat/prerun.py
+++ src/ansible_compat/prerun.py
@@ -11,7 +11,7 @@ def get_cache_dir(project_dir: Path) -> Path:
     # we would use the same key regardless the location of the user home
     # directory or where the project is clones (as long the project folder uses
     # the same name).
-    basename = project_dir.resolve().name.encode(encoding="utf-8")
+    basename = Path(project_dir).resolve().name.encode(encoding="utf-8")
     # 6 chars of entropy should be enough
     cache_key = hashlib.sha256(basename).hexdigest()[:6]
     cache_dir = (
